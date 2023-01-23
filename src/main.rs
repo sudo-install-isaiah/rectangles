e4a772e979474eb226457ac188e513e557ae7aca
zae <isaiah.iu@hotmail.com>
2023-01-23 10:22:26 -0500

    Use dbg! to view values in debugger

--- src/main.rs
+++ src/main.rs
@@ -5,16 +5,20 @@ struct Rectangle {
 }
 
 fn main() {
+    let scale = 2;
     let rect1 = Rectangle {
-        width: 30,
+        width: dbg!(30 * scale),
         height: 50,
     };
 
-    println!(
-        "The area of the rectangle is {} square pixels.", area(&rect1)
-    );
+//     println!(
+//         "The area of the rectangle is {} square pixels.", area(&rect1)
+//     );
 
-    println!("rect is {:?}", rect1);
+//     println!("rect is {:?}", rect1);
+
+    dbg!(&rect1);
+
 }
 
 fn area(rectangle: &Rectangle) -> u32 {
